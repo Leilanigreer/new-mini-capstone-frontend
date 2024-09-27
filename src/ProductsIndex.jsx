98ef07a5bf496966c7b4baba16849d062e5d12f2
Leilanigreer <leilani@lpcgolf.com>
2024-09-27 10:19:34 -0700

    Started adding the modal. Text is still hardcoded but button to open and close works.

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,4 +1,4 @@
-export function ProductsIndex({products}) {
+export function ProductsIndex({products, onShow}) {
   // console.log('hello')
   return(
     <div>
@@ -8,6 +8,7 @@ export function ProductsIndex({products}) {
           <h3>{product.name}</h3>
           <p>{product.price}</p>
           <img src={product.images[0] && product.images[0].url} />
+          <button onClick={() => onShow(product)}>More info</button>
         </div>
       )
     )}
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ProductsIndex } from './ProductsIndex';
 import { ProductsNew } from './ProductsNew';
+import { Modal } from './Model';
 
 export function ProductsPage() {
   const [products, setProducts] = useState([]);
+  const [isProductsShowVisible, setIsProductsShowVisible] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState({});
 
   const handleIndex = () => {
     axios.get("http://localhost:3000/products.json").then((response) => {
@@ -20,12 +23,26 @@ export function ProductsPage() {
     });
   };
 
+  const handleShow = ( product ) => {
+    console.log("handleShow", product);
+    setIsProductsShowVisible(true);
+    setCurrentProduct(product);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsProductsShowVisible(false);
+  };
+
   useEffect(handleIndex, []);
 
   return (
     <main>
       <ProductsNew onCreate={handleCreate} />
-      <ProductsIndex products={products} />
+      <ProductsIndex products={products} onShow={handleShow} />
+      <Modal show={isProductsShowVisible} onClose={handleClose}>
+        <h1>This is still a test</h1>
+      </Modal>
     </main>
   );
 }
