0c73496cfe3d24cd325caeacd9027e07297a05ab
Leilanigreer <leilani@lpcgolf.com>
2024-09-27 10:52:22 -0700

    added a form to update the product and show default values

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,4 +1,11 @@
-export function ProductsShow({ product }) {
+export function ProductsShow({ product, onUpdate }) {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    onUpdate(product.id, params, () => event.target.reset());
+  };
+  
   return (
     <div>
       <h1>Product information</h1>
@@ -6,6 +13,24 @@ export function ProductsShow({ product }) {
       <p>Price: {product.price}</p>
       <p>Description: {product.description}</p>
       <p>Supplier: {product.supplier.name}</p>
+      <br></br>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input defaultValue={product.name} name="name" type="text" />
+        </div>
+        <div>
+          Price: <input defaultValue={product.price} name="price" type="text" />
+        </div>
+        <div>
+          Description: <input defaultValue={product.description} name="description" type="text"/>
+        </div>
+        <div>
+          Supplier ID: <input defaultValue={product.supplier.id} name="supplier_id" type="text"/>
+        </div>
+        <div>
+          <button type="submit">Update</button>
+        </div>
+      </form>
     </div>
   );
 }
